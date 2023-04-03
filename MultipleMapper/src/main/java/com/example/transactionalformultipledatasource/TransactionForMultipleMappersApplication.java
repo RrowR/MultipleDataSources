0bc3bea2cd70cf17230ebb105e54529dbbf95b7e
Memo <2415782224@qq.com>
2023-04-03 22:16:44 +0800

    多mapper的数据源配置,麻烦

---
+++ MultipleMapper/src/main/java/com/example/transactionalformultipledatasource/TransactionForMultipleMappersApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionalformultipledatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionForMultipleMappersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionForMultipleMappersApplication.class, args);
+    }
+
+}
